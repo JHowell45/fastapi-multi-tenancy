d9659056254dd05fc73b01c32bef530b8ebdb4f0
= <jhowell45@googlemail.com>
2026-01-02 01:06:38 +0000

    function for updating the search_path.

--- app/deps/db.py
+++ app/deps/db.py
@@ -1,6 +1,8 @@
 from typing import Annotated, Generator
 
 from fastapi import Depends
+from sqlalchemy import Connection
+from sqlalchemy.sql import text
 from sqlmodel import Session, create_engine
 
 from .config import Settings, get_settings
@@ -16,3 +18,9 @@ def get_session() -> Generator[Session]:
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
+
+
+def update_search_path(conn: Connection, schemas: list[str]) -> None:
+    formatted_schemas: str = ", ".join(f'"{schema}"' for schema in schemas)
+    conn.execute(text(f"set search_path to {formatted_schemas};"))
+    conn.commit()
